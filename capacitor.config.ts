c7a9b3b4aab37f0168d37989d4d65155df98329d
Rakha Ramadhana <rakha.ramadhana@yahoo.com>
2025-11-11 08:14:38 +0800

    appName

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'sakinah-frozen-pos',
+  appName: 'Sakinah Frozen POS',
   webDir: 'dist'
 };
 
